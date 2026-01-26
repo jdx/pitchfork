a1116f29f4c4d018ad7210da7598cfc27514b0c3
gaojunran <nebula2021@126.com>
2026-01-26 16:53:07 +0800

    fix: refactor the logic of stopping a daemon and add tests

--- src/ipc/client.rs
+++ src/ipc/client.rs
@@ -282,11 +282,30 @@ impl IpcClient {
                 info!("stopped daemon {}", id);
                 Ok(())
             }
-            IpcResponse::DaemonAlreadyStopped => {
+            IpcResponse::DaemonNotRunning => {
                 warn!("daemon {} is not running", id);
                 Ok(())
             }
-            rsp => Err(Self::unexpected_response("Ok or DaemonAlreadyStopped", &rsp).into()),
+            IpcResponse::DaemonNotFound => {
+                warn!("daemon {} not found", id);
+                Ok(())
+            }
+            IpcResponse::DaemonWasNotRunning => {
+                warn!(
+                    "daemon {} was not running (process may have exited unexpectedly)",
+                    id
+                );
+                Ok(())
+            }
+            IpcResponse::DaemonStopFailed { error } => {
+                error!("failed to stop daemon {}: {}", id, error);
+                Ok(())
+            }
+            rsp => Err(Self::unexpected_response(
+                "Ok, DaemonNotRunning, DaemonNotFound, DaemonWasNotRunning, or DaemonStopFailed",
+                &rsp,
+            )
+            .into()),
         }
     }
 }
